21e576911c2cdbc034226ccf16c85cb9514ab20b
xd11cc <295900422@qq.com>
2025-06-13 14:57:57 +0800

    初始化项目

---
+++ src/main/java/com/xd11cc/single/SingleController.java
@@ -0,0 +1,16 @@
+package com.xd11cc.single;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: xd11cc
+ * @Date: 2025/6/13 14:24
+ **/
+@SpringBootApplication
+public class SingleController {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SingleController.class, args);
+    }
+}
---
+++ src/readme.md
@@ -0,0 +1,8 @@
+1、基础组织架构模块
+2、security整合安全框架
+3、freemarker整合代码生成器模版
+4、flowable整合工作流
+5、rabbitMQ整合消息队列
+6、xxl-job整合调度中心
+7、alipay&wxpay整合在线支付
+8、natty整合websocket消息推送
