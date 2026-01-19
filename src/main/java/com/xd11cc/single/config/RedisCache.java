ebd009536bdf6f8af81dd85bee5bd6091cc7a332
xd11cc <295900422@qq.com>
2026-01-19 23:19:53 +0800

    优化限流器+整合验证码工具

--- src/main/java/com/xd11cc/single/config/RedisCache.java
+++ src/main/java/com/xd11cc/single/config/RedisCache.java
@@ -1,6 +1,5 @@
 package com.xd11cc.single.config;
 
-import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.BoundSetOperations;
 import org.springframework.data.redis.core.HashOperations;
