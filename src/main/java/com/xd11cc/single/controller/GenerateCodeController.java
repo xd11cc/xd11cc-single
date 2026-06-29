d136d77766502d8dba43ba935788aa1603d63055
xd11cc <295900422@qq.com>
2026-06-29 16:29:32 +0800

    优化项目

--- src/main/java/com/xd11cc/single/controller/GenerateCodeController.java
+++ src/main/java/com/xd11cc/single/controller/GenerateCodeController.java
@@ -10,6 +10,7 @@ import com.xd11cc.single.utils.PageUtils;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -35,6 +36,7 @@ public class GenerateCodeController {
 
     @ApiOperation("生成代码")
     @GetMapping
+    @PreAuthorize("ss.hasPermission('system:generate:code')")
     public ResponseVO<List<PreviewCodeVO>> generateCode(String tableName){
         return ResponseVO.success(generateCodeService.generateCode(tableName));
     }
